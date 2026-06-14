8a57db9282398b2a11400df19b7e357499155d1b
Quanh <thaimap667jack@gmail.com>
2026-06-14 14:47:46 +0700

    build: update application ID and versioning
    Change the application ID to comply with new branding and reset the version name to 0.1 for the initial release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,11 +11,11 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.customnotifier.vxrmpa"
+    applicationId = "com.cnotifi.xounzii"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
-    versionName = "1.0"
+    versionName = "0.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
